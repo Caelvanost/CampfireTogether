29203f0d86b669b1e64e74860334a3ad30134872
Caelvanost <caelvanost@gmail.com>
2026-09-05 11:58:04 +0200

    feat: expose Papyrus remote materialization callbacks

--- src/PapyrusBridge.cpp
+++ src/PapyrusBridge.cpp
@@ -8,6 +8,11 @@ namespace CampfireTogether::PapyrusBridge
 {
     namespace
     {
+        [[nodiscard]] std::uint32_t RequestID(std::int32_t requestID)
+        {
+            return requestID > 0 ? static_cast<std::uint32_t>(requestID) : 0;
+        }
+
         void ReportPlaced(
             RE::StaticFunctionTag*,
             RE::TESObjectREFR* placedRef,
@@ -57,6 +62,54 @@ namespace CampfireTogether::PapyrusBridge
             return CampfireSync::GetSingleton().IsRemoteCampObject(reference);
         }
 
+        bool IsRemoteMaterializationRequestValid(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().IsRemoteMaterializationRequestValid(RequestID(requestID));
+        }
+
+        float GetRemoteMaterializationX(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().GetRemoteMaterializationX(RequestID(requestID));
+        }
+
+        float GetRemoteMaterializationY(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().GetRemoteMaterializationY(RequestID(requestID));
+        }
+
+        float GetRemoteMaterializationZ(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().GetRemoteMaterializationZ(RequestID(requestID));
+        }
+
+        float GetRemoteMaterializationAngleX(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().GetRemoteMaterializationAngleX(RequestID(requestID));
+        }
+
+        float GetRemoteMaterializationAngleY(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().GetRemoteMaterializationAngleY(RequestID(requestID));
+        }
+
+        float GetRemoteMaterializationAngleZ(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            return CampfireSync::GetSingleton().GetRemoteMaterializationAngleZ(RequestID(requestID));
+        }
+
+        void ReportRemoteMaterialized(
+            RE::StaticFunctionTag*,
+            std::int32_t requestID,
+            RE::TESObjectREFR* reference)
+        {
+            CampfireSync::GetSingleton().CompleteRemoteMaterialization(RequestID(requestID), reference);
+        }
+
+        void ReportRemoteMaterializationFailed(RE::StaticFunctionTag*, std::int32_t requestID)
+        {
+            CampfireSync::GetSingleton().FailRemoteMaterialization(RequestID(requestID));
+        }
+
         bool ConsumeLocalBuildIntent(RE::StaticFunctionTag*)
         {
             return LocalBuildIntent::Consume();
@@ -80,7 +133,7 @@ namespace CampfireTogether::PapyrusBridge
 
         void BridgeReady(RE::StaticFunctionTag*)
         {
-            SKSE::log::info("CFT PAPYRUS Campfire event listener READY");
+            SKSE::log::info("CFT PAPYRUS Campfire event listener READY remoteMaterialization=1");
         }
     }
 
@@ -93,12 +146,21 @@ namespace CampfireTogether::PapyrusBridge
         vm->RegisterFunction("ReportPlaced", "CampfireTogetherNative", ReportPlaced);
         vm->RegisterFunction("ReportRemoved", "CampfireTogetherNative", ReportRemoved);
         vm->RegisterFunction("IsRemoteCampObject", "CampfireTogetherNative", IsRemoteCampObject);
+        vm->RegisterFunction("IsRemoteMaterializationRequestValid", "CampfireTogetherNative", IsRemoteMaterializationRequestValid);
+        vm->RegisterFunction("GetRemoteMaterializationX", "CampfireTogetherNative", GetRemoteMaterializationX);
+        vm->RegisterFunction("GetRemoteMaterializationY", "CampfireTogetherNative", GetRemoteMaterializationY);
+        vm->RegisterFunction("GetRemoteMaterializationZ", "CampfireTogetherNative", GetRemoteMaterializationZ);
+        vm->RegisterFunction("GetRemoteMaterializationAngleX", "CampfireTogetherNative", GetRemoteMaterializationAngleX);
+        vm->RegisterFunction("GetRemoteMaterializationAngleY", "CampfireTogetherNative", GetRemoteMaterializationAngleY);
+        vm->RegisterFunction("GetRemoteMaterializationAngleZ", "CampfireTogetherNative", GetRemoteMaterializationAngleZ);
+        vm->RegisterFunction("ReportRemoteMaterialized", "CampfireTogetherNative", ReportRemoteMaterialized);
+        vm->RegisterFunction("ReportRemoteMaterializationFailed", "CampfireTogetherNative", ReportRemoteMaterializationFailed);
         vm->RegisterFunction("ConsumeLocalBuildIntent", "CampfireTogetherNative", ConsumeLocalBuildIntent);
         vm->RegisterFunction("ReportRemoteBuildSuppressed", "CampfireTogetherNative", ReportRemoteBuildSuppressed);
         vm->RegisterFunction("ReportRemoteBedrollAccess", "CampfireTogetherNative", ReportRemoteBedrollAccess);
         vm->RegisterFunction("BridgeReady", "CampfireTogetherNative", BridgeReady);
 
-        SKSE::log::info("CFT PAPYRUS native bridge READY class=CampfireTogetherNative");
+        SKSE::log::info("CFT PAPYRUS native bridge READY class=CampfireTogetherNative remoteMaterialization=1");
         return true;
     }
 }
