702065b2af0a5dd61b5a524504617aefb1b7f49e
Caelvanost <caelvanost@gmail.com>
2026-09-05 12:03:12 +0200

    build: package remote materialization bridge

--- build/_release.bat
+++ build/_release.bat
@@ -79,6 +79,8 @@ mkdir "%PACKAGE%\Scripts" >nul 2>&1
 if errorlevel 1 exit /b 1
 "%COMPILER%" "%CFT_SOURCE%\CampfireTogetherBridge.psc" -f="%FLAGS%" -i="%PAPYRUS_IMPORTS%" -o="%PACKAGE%\Scripts"
 if errorlevel 1 exit /b 1
+"%COMPILER%" "%CFT_SOURCE%\CampfireTogetherRemoteBridge.psc" -f="%FLAGS%" -i="%PAPYRUS_IMPORTS%" -o="%PACKAGE%\Scripts"
+if errorlevel 1 exit /b 1
 "%COMPILER%" "%OVERRIDE_SOURCE%\_Camp_SpawnCampfire.psc" -f="%FLAGS%" -i="%PAPYRUS_IMPORTS%" -o="%PACKAGE%\Scripts"
 if errorlevel 1 exit /b 1
 "%COMPILER%" "%OVERRIDE_SOURCE%\_Camp_CampTentNPCBedrollScript.psc" -f="%FLAGS%" -i="%PAPYRUS_IMPORTS%" -o="%PACKAGE%\Scripts"
@@ -90,6 +92,10 @@ if not exist "%PACKAGE%\Scripts\CampfireTogetherNative.pex" (
 )
 if not exist "%PACKAGE%\Scripts\CampfireTogetherBridge.pex" (
     echo ERROR: CampfireTogetherBridge.pex was not produced.
+    exit /b 1
+)
+if not exist "%PACKAGE%\Scripts\CampfireTogetherRemoteBridge.pex" (
+    echo ERROR: CampfireTogetherRemoteBridge.pex was not produced.
     exit /b 1
 )
 if not exist "%PACKAGE%\Scripts\_Camp_SpawnCampfire.pex" (
@@ -143,6 +149,7 @@ echo Build complete:
 echo   DLL: %PACKAGE%\SKSE\Plugins\CampfireTogether.dll
 echo   PEX: %PACKAGE%\Scripts\CampfireTogetherNative.pex
 echo   PEX: %PACKAGE%\Scripts\CampfireTogetherBridge.pex
+echo   PEX: %PACKAGE%\Scripts\CampfireTogetherRemoteBridge.pex
 echo   PEX: %PACKAGE%\Scripts\_Camp_SpawnCampfire.pex
 echo   PEX: %PACKAGE%\Scripts\_Camp_CampTentNPCBedrollScript.pex
 echo   ESP: %PACKAGE%\CampfireTogether.esp
