d2fa7b2a1eaf7f4a6e243a7b1acf5f8f0fc63203
Caelvanost <caelvanost@gmail.com>
2026-09-11 20:17:54 +0200

    refactor: generalize local Campfire power intent

--- src/LocalBuildIntent.h
+++ src/LocalBuildIntent.h
@@ -4,5 +4,10 @@ namespace CampfireTogether::LocalBuildIntent
 {
     void RegisterInputSink();
     void Reset();
+
+    // Backward-compatible Build Campfire helper.
     [[nodiscard]] bool Consume();
+
+    [[nodiscard]] bool ConsumePower(std::string_view powerTag, RE::Actor* caster);
+    [[nodiscard]] bool AuthorizeNestedPower(std::string_view powerTag);
 }
